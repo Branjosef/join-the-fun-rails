b3c984e02b8690958effdb5a5ecaf9207732c0f4
Brandon Fonnemann <branjosef@gmail.com>
2020-10-13 10:36:29 -0500

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,7 +1,7 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-      t.integer :passenger_id
+      t.integer :passenger_id  #you need to add these manually before db:migrate for association to work properly
       t.integer :taxi_id
       t.timestamps null: false
     end
